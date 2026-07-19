cce1dc8302cf60f29a5cf776fe5eb27de257659b
john-mark-s <120572941+john-mark-s@users.noreply.github.com>
2026-07-19 03:09:18 +0200

    Update data.js

--- data.js
+++ data.js
@@ -11445,12 +11445,13 @@ var D=[
       ],
       "beschlusstext": "Die BVV hat beschlossen: Der Antrag ist durch Amtshandeln erledigt.",
       "beschlussdatum": "07.12.2022",
+      "erledigte_art_wie": "geprueft",
+      ""erledigte_art_ergebniss": "vollstaendig-umgesetzt",
       "originalinitiator": "Linksfraktion",
       "originalparties": [
         "Linke"
       ]
     },
-    "sonstiges-unklar"
   ],
   [
     "236/VI",
